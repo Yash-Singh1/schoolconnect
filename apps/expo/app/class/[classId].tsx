de8217e149e06498f1ec29563fbac7c914454aed
Yash Singh <saiansh2525@gmail.com>
2023-04-22 08:25:42 -0700

    feat: image pan/zoom postcard

--- apps/expo/app/class/[classId].tsx
+++ apps/expo/app/class/[classId].tsx
@@ -1,6 +1,15 @@
 // Class page, shows posts and allows teachers to post
 
-import { Dimensions, Image, Text, TouchableOpacity, View } from "react-native";
+import {
+  Dimensions,
+  Image,
+  Modal,
+  Platform,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import ImageZoom from "react-native-image-pan-zoom";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Stack, useRouter, useSearchParams } from "expo-router";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
@@ -12,6 +21,7 @@ import { useAtom } from "jotai";
 
 import "dayjs/locale/en";
 import { useRef, useState } from "react";
+import { StatusBar } from "expo-status-bar";
 
 import LoadingWrapper from "../../src/components/LoadingWrapper";
 import { Navbar } from "../../src/components/Navbar";
@@ -26,12 +36,19 @@ dayjs.locale("en");
 const PostCard: React.FC<{ item: Post & { author: User } }> = ({ item }) => {
   // NOTE: We don't want to do any queries here because that would lead us to the n+1 query problem
 
+  // State for resizing the image (we can't anticipate aspect ratio, so expect layout shifts)
   const imageRef = useRef<Image | null>(null);
   const [width, setWidth] = useState<number | null>(null);
+  const [realWidth, setRealWidth] = useState<number | null>(null);
+  const [realHeight, setRealHeight] = useState<number | null>(null);
+
+  // Whether or not the pan/zoom modal is open
+  const [modalOpen, setModalOpen] = useState(false);
 
   return (
     <TouchableOpacity
       activeOpacity={0.5}
+      onPress={() => setModalOpen(true)}
       className="mx-4 rounded-lg border-2 border-violet-400/50 bg-violet-400/40"
     >
       <View className="flex gap-y-2 px-4 py-2">
@@ -48,15 +65,56 @@ const PostCard: React.FC<{ item: Post & { author: User } }> = ({ item }) => {
               onLoadEnd={() => {
                 // Some ratio algebra to get the width of the image with the aspect ratio in place
                 Image.getSize(item.image!, (width, height) => {
-                  setWidth((Dimensions.get('screen').height / 10) / height * width);
+                  setWidth(
+                    (Dimensions.get("screen").height / 10 / height) * width,
+                  );
+                  setRealWidth(width);
+                  setRealHeight(height);
                 });
               }}
               ref={imageRef}
               style={{ width: width || "100%" }}
               source={{ uri: item.image }}
               alt={item.title}
-              className="h-[10vh] flex-grow"
+              className={`h-[10vh] flex-grow`}
             />
+            <Modal visible={modalOpen} transparent={true}>
+              <SafeAreaView className="bg-slate-900">
+                <StatusBar
+                  style={Platform.OS === "android" ? "dark" : "light"}
+                />
+                <View className="w-full h-full flex justify-center items-center">
+                  <View className="flex items-end w-full px-4 mt-4">
+                    <TouchableOpacity
+                      onPress={() => setModalOpen(false)}
+                      activeOpacity={0.5}
+                    >
+                      <FontAwesomeIcon
+                        icon="square-xmark"
+                        color="white"
+                        size={20}
+                      />
+                    </TouchableOpacity>
+                  </View>
+                  {/* @ts-expect-error -- Typings don't contain children prop, need to patch that */}
+                  <ImageZoom
+                    cropWidth={Dimensions.get("window").width}
+                    cropHeight={Dimensions.get("window").height - 50}
+                    imageWidth={realWidth!}
+                    imageHeight={realHeight!}
+                  >
+                    <Image
+                      style={{
+                        width: realWidth!,
+                        height: realHeight!,
+                      }}
+                      source={{ uri: item.image }}
+                      alt={item.title}
+                    />
+                  </ImageZoom>
+                </View>
+              </SafeAreaView>
+            </Modal>
           </View>
         )}
         <Text className="text-xs sm:text-sm font-medium italic text-white">
